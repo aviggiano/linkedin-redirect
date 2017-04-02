61801f93bd1b1372f1e2f1c8cb6ee5518946a012
Antonio Viggiano <agfviggiano@gmail.com>
2017-04-02 13:21:55 -0300

    fixes OPNESHIFT variables

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const http= require('http')
 const app = express() 
 const LINKEDIN_URL = 'https://linkedin.com/in/aviggiano'
-const PORT = 8080
+const PORT = 3000
+const IP = '127.0.0.1'
 
-app.set('port', process.env.PORT || PORT)
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || PORT);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || IP);
 
-const httpServer = http.createServer(app)
-httpServer.listen(app.get('port'))
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+});
+
 
 app.get('*', function(req, res) {
 	res.redirect(LINKEDIN_URL); 
